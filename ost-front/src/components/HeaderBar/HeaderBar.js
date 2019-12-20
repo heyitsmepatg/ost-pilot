6af1004fbc3083c973662465aa2776f5ed4088c9
Hafiz Aina <hafizaina94@gmail.com>
2019-12-20 09:59:39 -0500

    added links to header

--- ost-front/src/components/HeaderBar/HeaderBar.js
+++ ost-front/src/components/HeaderBar/HeaderBar.js
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom';
 import React, {Component} from 'react';
 import './HeaderBar.css';
 
@@ -11,8 +12,12 @@ export default class HeaderBar extends Component {
                         <span className='align--left'>Home</span>
                         <span className='align--right'>
                             <ul>
-                                <li>Employee Log In</li>
-                                <li>Citizen Log In</li>
+                                <li>
+                                    <Link to='#'>Employee Log In</Link>
+                                </li>
+                                <li>
+                                    <Link to='#'>Citizen Log In</Link>
+                                </li>
                             </ul>
                         </span>
                     </div>
